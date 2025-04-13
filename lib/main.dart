9a9f329c78a2f92b97440c486f72c40faf0234d1
Areta Arwaa <aretaathayaa@gmail.com>
2025-04-13 17:28:45 +0700

    add: local database

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'todo_item.dart';
 import 'task_tile.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(TodoItemAdapter());
+  await Hive.openBox<TodoItem>('myBox');
+
   runApp(const TodoApp());
 }
 
@@ -26,35 +32,26 @@ class TodoScreen extends StatefulWidget {
 }
 
 class TodoScreenState extends State<TodoScreen> {
+  final Box<TodoItem> _myBox = Hive.box<TodoItem>('myBox');
+
   List<String> categories = ["Today", "Tomorrow", "Next Week", "Next Month"];
   int selectedCategory = 0;
 
-  List<TodoItem> tasks = [
-    TodoItem(title: "Upload a Video", completed: false, category: "Today"),
-    TodoItem(title: "Go to the Gym", completed: false, category: "Tomorrow"),
-    TodoItem(title: "Go to Office", completed: true, category: "Next Week"),
-    TodoItem(title: "Complete a Task", completed: true, category: "Next Month"),
-    TodoItem(title: "Go to the Market", completed: false, category: "Today"),
-  ];
-
   List<TodoItem> get filteredTasks {
-    return tasks
+    return _myBox.values
         .where((task) => task.category == categories[selectedCategory])
         .toList();
   }
 
-  void _toggleTask(int index) {
-    int originalIndex = tasks.indexOf(filteredTasks[index]);
+  void _toggleTask(TodoItem task) {
     setState(() {
-      tasks[originalIndex].completed = !tasks[originalIndex].completed;
+      task.completed = !task.completed;
+      task.save();
     });
   }
 
-  void _editTask(int index) {
-    int originalIndex = tasks.indexOf(filteredTasks[index]);
-    TextEditingController controller = TextEditingController(
-      text: tasks[originalIndex].title,
-    );
+  void _editTask(TodoItem task) {
+    TextEditingController controller = TextEditingController(text: task.title);
     showDialog(
       context: context,
       builder:
@@ -72,7 +69,8 @@ class TodoScreenState extends State<TodoScreen> {
               ElevatedButton(
                 onPressed: () {
                   setState(() {
-                    tasks[originalIndex].title = controller.text;
+                    task.title = controller.text;
+                    task.save();
                   });
                   Navigator.pop(context);
                 },
@@ -83,10 +81,9 @@ class TodoScreenState extends State<TodoScreen> {
     );
   }
 
-  void _deleteTask(int index) {
-    int originalIndex = tasks.indexOf(filteredTasks[index]);
+  void _deleteTask(TodoItem task) {
     setState(() {
-      tasks.removeAt(originalIndex);
+      task.delete();
     });
   }
 
@@ -109,14 +106,13 @@ class TodoScreenState extends State<TodoScreen> {
               ElevatedButton(
                 onPressed: () {
                   if (controller.text.isNotEmpty) {
+                    final newTask = TodoItem(
+                      title: controller.text,
+                      completed: false,
+                      category: categories[selectedCategory],
+                    );
                     setState(() {
-                      tasks.add(
-                        TodoItem(
-                          title: controller.text,
-                          completed: false,
-                          category: categories[selectedCategory],
-                        ),
-                      );
+                      _myBox.add(newTask);
                     });
                   }
                   Navigator.pop(context);
@@ -132,120 +128,124 @@ class TodoScreenState extends State<TodoScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.only(
-              top: 60,
-              left: 20,
-              right: 20,
-              bottom: 20,
-            ),
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.blue.shade900, Colors.blue.shade300],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              ),
-              borderRadius: const BorderRadius.vertical(
-                bottom: Radius.circular(30),
-              ),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Task Manager",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
+      body: ValueListenableBuilder(
+        valueListenable: _myBox.listenable(),
+        builder: (context, box, _) {
+          return Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.only(
+                  top: 60,
+                  left: 20,
+                  right: 20,
+                  bottom: 20,
+                ),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.blue.shade900, Colors.blue.shade300],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                  borderRadius: const BorderRadius.vertical(
+                    bottom: Radius.circular(30),
                   ),
                 ),
-                const SizedBox(height: 5),
-                const Text(
-                  "Manage your tasks easily",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 4, 41, 60),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: List.generate(categories.length, (index) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 5),
-                        child: ChoiceChip(
-                          label: Text(categories[index]),
-                          selected: selectedCategory == index,
-                          onSelected: (bool selected) {
-                            setState(() {
-                              selectedCategory = index;
-                            });
-                          },
-                          selectedColor: const Color.fromARGB(
-                            255,
-                            64,
-                            141,
-                            205,
-                          ),
-                          backgroundColor: Colors.grey[200],
-                          labelStyle: TextStyle(
-                            color:
-                                selectedCategory == index
-                                    ? Colors.white
-                                    : Colors.black,
-                          ),
-                        ),
-                      );
-                    }),
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          Expanded(
-            child: ListView.builder(
-              padding: const EdgeInsets.all(20),
-              itemCount: filteredTasks.length,
-              itemBuilder: (context, index) {
-                return TaskTile(
-                  task: filteredTasks[index],
-                  onToggle: () => _toggleTask(index),
-                  onEdit: () => _editTask(index),
-                  onDelete: () => _deleteTask(index),
-                );
-              },
-            ),
-          ),
-
-          Container(
-            width: double.infinity,
-            margin: const EdgeInsets.all(20),
-            child: ElevatedButton(
-              onPressed: _addTask,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Task Manager",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    const Text(
+                      "Manage your tasks easily",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 4, 41, 60),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: List.generate(categories.length, (index) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 5),
+                            child: ChoiceChip(
+                              label: Text(categories[index]),
+                              selected: selectedCategory == index,
+                              onSelected: (bool selected) {
+                                setState(() {
+                                  selectedCategory = index;
+                                });
+                              },
+                              selectedColor: const Color.fromARGB(
+                                255,
+                                64,
+                                141,
+                                205,
+                              ),
+                              backgroundColor: Colors.grey[200],
+                              labelStyle: TextStyle(
+                                color:
+                                    selectedCategory == index
+                                        ? Colors.white
+                                        : Colors.black,
+                              ),
+                            ),
+                          );
+                        }),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              child: const Text(
-                "Add a New Task",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+              Expanded(
+                child: ListView.builder(
+                  padding: const EdgeInsets.all(20),
+                  itemCount: filteredTasks.length,
+                  itemBuilder: (context, index) {
+                    final task = filteredTasks[index];
+                    return TaskTile(
+                      task: task,
+                      onToggle: () => _toggleTask(task),
+                      onEdit: () => _editTask(task),
+                      onDelete: () => _deleteTask(task),
+                    );
+                  },
                 ),
               ),
-            ),
-          ),
-        ],
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.all(20),
+                child: ElevatedButton(
+                  onPressed: _addTask,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child: const Text(
+                    "Add a New Task",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
--- lib/todo_item.dart
+++ lib/todo_item.dart
@@ -1,6 +1,15 @@
-class TodoItem {
+import 'package:hive/hive.dart';
+part 'todo_item.g.dart';
+
+@HiveType(typeId: 0)
+class TodoItem extends HiveObject {
+  @HiveField(0)
   String title;
+
+  @HiveField(1)
   bool completed;
+
+  @HiveField(2)
   String category;
 
   TodoItem({
